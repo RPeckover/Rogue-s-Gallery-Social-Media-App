1e89aa2adfaabde07a620ebe20126cf6b6533832
RPeckover <Henry.peckover19@bathspa.ac.uk>
2026-04-27 20:23:41 +0100

    Comment functionality
    Edited 'posts-data.js' to bring it in-line with Week 10 materials. Changed method of concatenation in 'dataplan.js'. Modified some navigation text.

--- dataplan.js
+++ dataplan.js
@@ -249,17 +249,14 @@ let nouns = [
 //   promptSelectThree() {}
 // ];
 
-//Would a switch statment be better as I could randomise which case (function) is used
 // and then make it always run the code to check the usedPrompts and add new successful prompts to it
 
 let randomNoun = nouns[Math.floor(Math.random() * nouns.length)];
 let randomAdjective = adjectives[Math.floor(Math.random() * adjectives.length)];
 
-let promptOne = randomAdjective.concat(" ", randomNoun);
-let promptTwo = randomNoun.concat(" ", randomNoun, " fusion");
+let promptOne = randomAdjective + " " + randomNoun;
+let promptTwo = randomNoun + " " + randomNoun + " " + fusion;
 // test to see if this just will print two of the same nouns
-
-//maybe try functions called using a number in an object?
 
 //array.rpototype.find() - array.find looking through usedPrompts, if match is found, returns True - we want it to return 'undefined'
 
--- posts-data.js
+++ posts-data.js
@@ -1,17 +1,76 @@
-// const posts=[]
+const mongoose=require('mongoose');
+const {Schema, model} = mongoose;
 
-// function addNewPost(userID, post){
-//     let myPost={
-//         postedBy: userID,
-//         message: post.message,
-//         likes: 0,
-//         time: Date.now()
-//     }
-//     posts.unshift(myPost)
-// }
+const postSchema = new Schema({
+    postedBy: String,
+    message: String,
+    imagePath: String,
+    likes: Number,
+    time: Date,
+    comments: [
+        {
+            message: String,
+            commentBy: String,
+            likes: Number
+        }
+    ]
+});
 
-// function getPosts(n=3){
-//     return posts.slice(0,n)
-// }
+const Posts = model('Post', postSchema);
 
-// module.exports={addNewPost, getPosts}
+function addNewPost(userID, post, imageFilename){
+    let myPost={
+        postedBy: userID,
+        message: post.message,
+        imagePath: imageFilename,
+        likes: 0,
+        time: Date.now(),
+        comments: []
+    }
+    Posts.create(myPost)
+        .catch(err=>{
+            console.log("Error: "+err)
+        })
+    // posts.unshift(myPost)
+}
+
+async function getPosts(n=3){
+    let data=[]
+    await Posts.find({})
+        .sort({'time': -1})
+        .limit(n)
+        .exec()
+        .then(mongoData=>{
+            data=mongoData
+        })
+    
+    return data
+}
+
+async function getPost(postID){
+    let foundPost=null
+    await Posts.findOne({_id:postID})
+        .exec()
+        .then(mongoData=>{
+            foundPost=mongoData
+        })
+    return foundPost
+}
+
+async function likePost(postID){
+    await Posts.findOneAndUpdate({_id:postID}, {$inc: {likes: 1}})
+    //Mongoose method for modifying and incrementing data
+        .exec()
+}
+
+async function commentOnPost(postID, commentText, commentBy){
+    let newComment={
+        message: commentText,
+        commentBy: commentBy,
+        likes: 0
+    }
+    await Posts.findOneAndUpdate({_id:postID}, {$push: {comments: newComment}})
+        .exec()
+}
+
+module.exports={addNewPost, getPosts, getPost, likePost, commentOnPost}
